422e24081ae5d845356719a2e9ea6df18d31ee12
JER.8789 <je.rogers8789@gmail.com>
2021-03-03 21:19:39 -0600

    .

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,7 @@ $("#currentDay").append(date);
 
 
 
-var timeOfday = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var timeOfday = ["9", "10", "11", "12", "1", "2", "3", "4", "5"]
 updatetime();
 
 function updatetime() {
