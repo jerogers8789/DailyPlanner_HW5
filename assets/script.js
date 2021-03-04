3deebd7ae1ed6e361e55bbc445f32701f685bc40
JER.8789 <je.rogers8789@gmail.com>
2021-03-03 21:53:25 -0600

    .

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,7 @@
 
 var date = moment().format("MMM Do YY");
 
-
 $("#currentDay").append(date);
-
-
-
 
 var timeOfday = ["9", "10", "11", "12", "1", "2", "3", "4", "5"]
 updatetime();
@@ -13,40 +9,27 @@ updatetime();
 function updatetime() {
   var currentTime = moment().format('H');
   for(var i = 0; i < timeOfday.length; i++) {
-
     if (parseInt(timeOfday[i]) > currentTime) {
-      $("#" + timeOfday[i]).attr("style", "background-color: #58ce7b");}
+      $("#" + timeOfday[i]).attr("style", "background-color: white");}
     else if (parseInt(timeOfday[i]) < currentTime) {
       $("#" + timeOfday[i]).attr("style", "background-color: lightgray"); }
     else if (parseInt(timeOfday[i]) == currentTime) {
-      $("#" + timeOfday[i]).attr("style", "background-color: #fc665e"); }
+      $("#" + timeOfday[i]).attr("style", "background-color: chartreuse"); }
   }
 }
-
 
 $(".rowBtn").on("click", function() {
     var timeOfday = $(this).parent().attr("id");
     var textContent = $("input").val().trim();
-
     localStorage.setItem(timeOfday, textContent);
     console.log(timeOfday, textContent);
 });
-
-
-  $("#9am").children("input").val(localStorage.getItem("9am"));
-
-  $("#10am").children("input").val(localStorage.getItem("10am"));
-
-  $("#11am").children("input").val(localStorage.getItem("11am"));
-
-  $("#12pm").children("input").val(localStorage.getItem("12pm"));
-
-  $("#1pm").children("input").val(localStorage.getItem("1pm"));
-
-  $("#2pm").children("input").val(localStorage.getItem("2pm"));
-
-  $("#3pm").children("input").val(localStorage.getItem("3pm"));
-
-  $("#4pm").children("input").val(localStorage.getItem("4pm"));
-
-  $("#5pm").children("input").val(localStorage.getItem("5pm"));
+$("#9am").children("input").val(localStorage.getItem("9am"));
+$("#10am").children("input").val(localStorage.getItem("10am"));
+$("#11am").children("input").val(localStorage.getItem("11am"));
+$("#12pm").children("input").val(localStorage.getItem("12pm"));
+$("#1pm").children("input").val(localStorage.getItem("1pm"));
+$("#2pm").children("input").val(localStorage.getItem("2pm"));
+$("#3pm").children("input").val(localStorage.getItem("3pm"));
+$("#4pm").children("input").val(localStorage.getItem("4pm"));
+$("#5pm").children("input").val(localStorage.getItem("5pm"));
